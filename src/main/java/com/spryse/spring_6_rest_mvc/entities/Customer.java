719c519628d826fe355df6bf51ee629aff189be8
Jonathan Braat <jonathan.braat@live.com>
2025-10-19 21:59:54 +1300

    [ADD] v2 migration, email column to table and entity

--- src/main/java/com/spryse/spring_6_rest_mvc/entities/Customer.java
+++ src/main/java/com/spryse/spring_6_rest_mvc/entities/Customer.java
@@ -23,6 +23,7 @@ public class Customer {
     @Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false)
     private UUID id;
     private String customerName;
+    private String email;
     @Version
     private Integer version;
     private LocalDateTime createdDate;
