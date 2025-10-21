798bc131e4be0aa074f8809d7f6aab94e557548e
Jonathan Braat <jonathan.braat@live.com>
2025-10-21 13:16:53 +1300

    [MOD] mysql container service connection instead of dynamic property source

--- src/test/java/com/spryse/spring_6_rest_mvc/repositories/MySqlTest.java
+++ src/test/java/com/spryse/spring_6_rest_mvc/repositories/MySqlTest.java
@@ -4,6 +4,7 @@ import com.spryse.spring_6_rest_mvc.entities.Beer;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
@@ -21,17 +22,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 @ActiveProfiles("localmysql")
 public class MySqlTest {
     @Container
+    @ServiceConnection
     static MySQLContainer<?> mySQLContainer = new MySQLContainer<>("mysql:9.2");
 
+    /*
     @DynamicPropertySource
     static void mySqlProperties(DynamicPropertyRegistry registry) {
         registry.add("spring.datasource.username", mySQLContainer::getUsername);
         registry.add("spring.datasource.password", mySQLContainer::getPassword);
         registry.add("spring.datasource.url", mySQLContainer::getJdbcUrl);
     }
-
-    @Autowired
-    DataSource dataSource;
+    */
 
     @Autowired
     BeerRepository beerRepository;
